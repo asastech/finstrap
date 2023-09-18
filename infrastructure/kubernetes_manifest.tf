a0c08c7dc285235f7e30655057b52bd3d1cf85a2
Mohamed Ahmed <softtechsd@gmail.com>
2023-09-19 00:29:59 +0300

    first commit

--- infrastructure/kubernetes_manifest.tf
+++ infrastructure/kubernetes_manifest.tf
@@ -1,9 +1,4 @@
 
-resource "kubernetes_manifest" "finstrap" {
-
-  yaml_body = file("${path.module}/finstrap/finstrap.yaml")
-}
-
 data "external" "kubectl_apply" {
   program = ["kubectl", "apply", "-f", "${path.module}/finstrap/finstrap.yaml"]
 }
