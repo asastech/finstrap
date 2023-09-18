7ec80f970c7c865f55ac526a46dd7c5911f8fd67
Mohamed Ahmed <softtechsd@gmail.com>
2023-09-19 00:37:04 +0300

    first commit

--- infrastructure/kubernetes_manifest.tf
+++
@@ -1,4 +0,0 @@
-
-data "external" "kubectl_apply" {
-  program = ["kubectl", "apply", "-f", "${path.module}/finstrap/finstrap.yaml"]
-}
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,14 @@
+# Provider Configuration (Kubernetes)
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Path to your kubeconfig file
-} 
+  config_path = "~/.kube/config"  # Update with your kubeconfig path
+}
+
+# Kubernetes Manifest Resource
+resource "kubernetes_manifest" "my_app" {
+  yaml_body = file("${path.module}/finstrap/finstrap.yaml")  # Path to your YAML file
+}
+
+# External Data Source to Apply the Manifest Using kubectl
+data "external" "apply_manifest" {
+  program = ["kubectl", "apply", "-f", "${path.module}/finstrap/finstrap.yaml"]
+}
