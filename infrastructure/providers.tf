9ebbfbb571b55fb8434f0fcb918c9b105070bdef
Mohamed Ahmed <softtechsd@gmail.com>
2023-09-19 00:49:04 +0300

    first commit

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,10 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.94.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.1.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+  }
+}
+
 # Provider Configuration (Kubernetes)
 provider "kubernetes" {
   config_path = "~/.kube/config"  # Update with your kubeconfig path
 }
 
 # Kubernetes Manifest Resource
-resource "kubernetes_manifest" "my_app" {
+resource "kubernetes_manifest" "finstrap" {
   yaml_body = file("${path.module}/finstrap/finstrap.yaml")  # Path to your YAML file
 }
 
