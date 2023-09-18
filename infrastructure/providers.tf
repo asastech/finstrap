b4e2ab57596e9f72f69530fc762a1619a886aced
Mohamed Ahmed <softtechsd@gmail.com>
2023-09-19 01:01:31 +0300

    first commit

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.94.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.3"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.1.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
-  }
-}
-
 # Provider Configuration (Kubernetes)
 provider "kubernetes" {
   config_path = "~/.kube/config"  # Update with your kubeconfig path
